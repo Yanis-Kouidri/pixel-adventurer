9f47c7194f28b841fc2077cec1ad514cf6ed896f
cedric <cedric.abdelbaki@etu.inp-toulouse.fr>
2023-05-28 20:26:06 +0200

    Feature/Map Classe Map.java v0.1 Corrections messages exceptions et commentaires

--- src/gameengine/map/model/Map.java
+++ src/gameengine/map/model/Map.java
@@ -25,7 +25,7 @@ public class Map {
 	// The 2D array of tiles representing the map object
 	private Tile[][] array;
 
-	/** Initializes a map object with a name, a biome, a width and a height
+	/** Initializes a map object with a name, a type, a width and a height
 	 * @param name The desired map name
 	 * @param type The desired map type
 	 * @param width The desired map width
@@ -34,13 +34,13 @@ public class Map {
 	 * @param scale The desired scale value for procedural generation
 	 */
 	public Map (String name, MapType type, int width, int height, double amplitude, double scale) {
-		this.name = name ;
-		this.type = type ;
-		this.width = width ;
-		this.height = height ;
+		this.name = name;
+		this.type = type;
+		this.width = width;
+		this.height = height;
 		try {
 			this.seed = System.currentTimeMillis();
-			this.array = MapArray.generateMapArray(this.type, this.width, this.height, this.seed, amplitude, scale) ;
+			this.array = MapArray.generateMapArray(this.type, this.width, this.height, this.seed, amplitude, scale);
 		} catch (Exception e) {
 			System.out.println("Error while generating the map");
 		}
@@ -50,35 +50,35 @@ public class Map {
 	 * @return this.name The map name
 	 */
 	public String getMapName() {
-		return this.name ;
+		return this.name;
 	}
 
 	/** Gets the map type
 	 * @return this.type The map type
 	 */
 	public MapType getMapType() {
-		return this.type ;
+		return this.type;
 	}
 
 	/** Gets the map width
 	 * @return this.width The map width
 	 */
 	public int getMapWidth() {
-		return this.width ;
+		return this.width;
 	}
 
 	/** Gets the map height
 	 * @return this.height The map height
 	 */
 	public int getMapHeight() {
-		return this.height ;
+		return this.height;
 	}
 
 	/** Gets the map array
 	 * @return this.array The map array
 	 */
 	public Tile[][] getMapArray() {
-		return this.array ;
+		return this.array;
 	}
 
 	/** Gets the tile at a specified position
@@ -88,7 +88,7 @@ public class Map {
 		try {
 			return this.array[x][y];
 		} catch (ArrayIndexOutOfBoundsException e) {
-			System.out.println("to do");
+			System.out.println("Specified parameters provoke an array index out of bounds error");
 			return null;
 		}
 	}
@@ -103,7 +103,7 @@ public class Map {
 			this.array[x][y] = null;
 			this.array[x][y] = tileToSet;
 		} catch (ArrayIndexOutOfBoundsException e) {
-			System.out.println("to do");
+			System.out.println("Specified parameters provoke an array index out of bounds error");
 		}
 	}
 }
