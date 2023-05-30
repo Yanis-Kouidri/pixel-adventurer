2bf62a77da18219035dcc413a3db432157540388
Yanis Kouidri <yanis.kouidri@gmail.com>
2023-05-30 08:43:27 +0200

    First inventory test file at correct emplacement

---
+++ test/gameengine/inventory/InventoryTest.java
@@ -0,0 +1,50 @@
+package gameengine.inventory;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class InventoryTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void getNumberOfPlace() {
+    }
+
+    @Test
+    void getNumberOfItems() {
+    }
+
+    @Test
+    void isFull() {
+    }
+
+    @Test
+    void isEmpty() {
+    }
+
+    @Test
+    void remove() {
+    }
+
+    @Test
+    void getFirstEmptyPlace() {
+    }
+
+    @Test
+    void add() {
+    }
+
+    @Test
+    void getItemByIndex() {
+    }
+}
