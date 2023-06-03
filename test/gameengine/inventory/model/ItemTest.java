3656b4e977b1b9c48e67ab18272f7129ee3177d8
Yanis Kouidri <yanis.kouidri@gmail.com>
2023-06-03 10:25:53 +0200

    Adding few tests for Item class

--- test/gameengine/inventory/model/ItemTest.java
+++ test/gameengine/inventory/model/ItemTest.java
@@ -4,27 +4,49 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import javax.swing.*;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class ItemTest {
 
+    private Item lemon;
+    private ImageIcon lemonIcon;
+    private JLabel lemonJlabel;
+    private String lemonDesc;
+    private String lemonName;
+
     @BeforeEach
     void setUp() {
+        lemonIcon = new ImageIcon("test/gameassets/item/lemon.png");
+        lemonJlabel = new JLabel(lemonIcon);
+        lemonDesc = "When life give you lemons...";
+        lemonName = "Lemon";
+
+        lemon = new Item(lemonName, lemonJlabel, lemonDesc);
     }
 
     @AfterEach
     void tearDown() {
+        lemonIcon = null;
+        lemonJlabel = null;
+        lemon = null;
+        lemonName = null;
+        lemonDesc = null;
     }
 
     @Test
     void getName() {
+        assertEquals(lemonName, lemon.getName());
     }
 
     @Test
     void getSprite() {
+        assertEquals(lemonJlabel, lemon.getSprite());
     }
 
     @Test
     void getDescription() {
+        assertEquals(lemonDesc, lemon.getDescription());
     }
 }
