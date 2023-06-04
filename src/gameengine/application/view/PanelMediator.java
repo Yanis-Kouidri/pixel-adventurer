a9d68c4056aca9390b8a1dc5da1c516ad56cdd54
cedric <cedric.abdelbaki@etu.inp-toulouse.fr>
2023-06-04 19:30:12 +0200

    Feature/Window Classe PanelMediator.java Modification de la classe (commandes, crédits, types)  ajout javadoc, methodes, attributs

--- src/gameengine/application/view/PanelMediator.java
+++ src/gameengine/application/view/PanelMediator.java
@@ -1,8 +1,33 @@
 package gameengine.application.view;
 
+/** Sets the differents panels
+ * @author Éric YU
+ * @contributor Cédric ABDELBAKI
+ *              - Added : javadoc
+ *              - Added : commandsPanel attribute
+ *              - Added : creditsPanel attribute
+ *              - Added : typesPanel attribute
+ *              - Added : setCommandsPanel() method
+ *              - Added : setCreditsPanel() method
+ *              - Added : setTypesPanel() method
+ */
+
 public class PanelMediator {
+
+	// The game panel
     private GamePanel gamePanel;
+
+    // The main menu panel
     private MenuPanel menuPanel;
+
+    // The commands information panel
+    private CommandsPanel commandsPanel;
+
+	// The credits panel
+	private CreditsPanel creditsPanel;
+
+    // The type selection panel for map creation
+    private TypesPanel typesPanel;
 
     public void setGamePanel(GamePanel gamePanel) {
         this.gamePanel = gamePanel;
@@ -12,6 +37,18 @@ public class PanelMediator {
         this.menuPanel = menuPanel;
     }
 
+    public void setCommandsPanel(CommandsPanel commandsPanel) {
+    	this.commandsPanel = commandsPanel;
+    }
+
+    public void setCreditsPanel(CreditsPanel creditsPanel) {
+    	this.creditsPanel = creditsPanel;
+    }
+
+    public void setSelectType(TypesPanel typesPanel) {
+    	this.typesPanel = typesPanel;
+    }
+
     public GamePanel getGamePanel() {
         return (gamePanel);
     }
@@ -19,5 +56,5 @@ public class PanelMediator {
     public MenuPanel getMenuPanel() {
         return (menuPanel);
     }
-
 }
+
