3bf59ce429fd4e37228dbb7fc2152ce3f6dd60d5
cedric <cedric.abdelbaki@etu.inp-toulouse.fr>
2023-06-05 15:07:22 +0200

    Feature/Window Classe MenuPanel.java v0.2 Modification de la classe, ajout modification et suppression javadoc, methodes, attributs

--- src/gameengine/utils/model/Constants.java
+++ src/gameengine/utils/model/Constants.java
@@ -1,6 +1,12 @@
 package gameengine.utils.model;
+import java.awt.Dimension;
 
-import java.util.logging.Logger;
+/** Stores all the constants that will be used in the application
+ * @author Jérémy THERET
+ * @contributor Cédric ABDELBAKI
+ * 				- Added : javadoc
+ * 				- Added : MenuPanel.java constants
+ */
 
 public class Constants {
     private Constants() {}
@@ -9,4 +15,25 @@ public class Constants {
 
     public static final int CHARACTER_LENGHT = 64;
 
+    /**
+     *  MenuPanel.java
+     */
+
+    // The game version
+ 	public static final String GAME_VERSION = "Alpha 0.1";
+
+ 	// The path to the title logo
+ 	public static final String LOGO_PATH = "src/gameassets/application/images/pixelAdventurerLogo.png";
+
+    // The path to the musical theme
+	public static final String THEME_PATH = "src/gameassets/application/sounds/pixelAdventurerTheme.wav";
+
+	// The path to the background image
+	public static final String BCKGRND_PATH = "src/gameassets/application/images/menuBackground.png";
+
+	// A constant for the menu panel dimensions
+	public static final Dimension PANEL_DIMENSIONS = new Dimension(1920, 1080);
+
+	// A constant for the menu panel empty borders
+	public static final int BORDER_CONST = 50;
 }
