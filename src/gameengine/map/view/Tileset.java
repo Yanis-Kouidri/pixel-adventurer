6bf45fa7827f6074d43285c26917e3328e7e1a29
Eric YU <yu.czeric@gmail.com>
2023-06-07 21:38:17 +0200

    created a cache for better performance

--- src/gameengine/map/view/Tileset.java
+++ src/gameengine/map/view/Tileset.java
@@ -5,6 +5,8 @@ import javax.imageio.ImageIO;
 import java.io.File;
 import java.io.IOException;
 import java.awt.Image;
+import java.util.HashMap;
+import java.util.Map;
 
 import static gameengine.utils.model.Constants.SPRITE_DIM;
 
@@ -17,7 +19,8 @@ public class Tileset {
 
 	// The tileset
 	private BufferedImage set;
-
+	private int nbTiles;
+	private Map<Integer, Image> cachedImages = new HashMap<>(); // The different tiles cached as images
 
 	/** Instantiates a Tileset object
 	 * @param tilesetPath The path to the tileset image
@@ -28,6 +31,8 @@ public class Tileset {
 		} catch (IOException e) {
 			System.out.println("Error trying to read the tileset image file");
 		}
+		nbTiles = computeNumberTiles();
+		createCache();
 	}
 
 	/** Gets a sprite in the tileset, using a tile identifier
@@ -35,16 +40,32 @@ public class Tileset {
 	 * @return sprite The tile sprite
 	 */
 	public Image getTileSprite(int tileIdentifier) {
-		try {
-			int numberOfColumns = set.getWidth() / SPRITE_DIM;
+		return cachedImages.get(tileIdentifier);
+	}
+
+	/**
+	 * Creates the cache images for less RAM usage.
+	 */
+	public void createCache() {
+		int numberOfColumns = set.getWidth() / SPRITE_DIM;
+		for (int tileIdentifier = 0; tileIdentifier < nbTiles; tileIdentifier++) {
 			int x = tileIdentifier / numberOfColumns;
 			int y = tileIdentifier % numberOfColumns;
 			Image sprite = set.getSubimage(y * SPRITE_DIM
 					, x * SPRITE_DIM, SPRITE_DIM, SPRITE_DIM);
-			return sprite;
-		} catch (Exception e) {
-			System.out.println("Error trying to get the sprite from the tileset");
-			return null;
+
+			cachedImages.put(tileIdentifier, sprite);
 		}
+	}
+
+	/**
+	 * Computes the number of tiles that can be extracted
+	 * from the given tileset image.
+	 * @return the number of tiles
+	 */
+	public int computeNumberTiles(){
+		int numberOfColumns = set.getWidth() / SPRITE_DIM;
+		int numberOfRows = set.getHeight() / SPRITE_DIM;
+		return numberOfRows*numberOfColumns;
 	}
 }
