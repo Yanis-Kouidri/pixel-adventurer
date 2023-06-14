8e4463eea7090847ab2108a02f6867e05aa5c2f3
cedric <cedric.abdelbaki@etu.inp-toulouse.fr>
2023-06-14 15:35:00 +0200

    Modified : MenuPanel.java abstract class, inherited by MainMenuPanel.java and ScrollPanePanel.java

--- src/gameengine/application/view/MenuPanel.java
+++ src/gameengine/application/view/MenuPanel.java
@@ -1,14 +1,13 @@
 package gameengine.application.view;
+import gameengine.application.model.CommandsMap;
 import gameengine.utils.model.Constants;
 import javax.swing.JFrame;
-import javax.swing.JPanel;
 import javax.swing.JLabel;
-import javax.swing.JButton;
 import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.SwingConstants;
 import javax.swing.border.EmptyBorder;
 import java.awt.Color;
-import java.awt.Component;
 import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.GridBagConstraints;
@@ -19,172 +18,43 @@ import java.awt.event.MouseListener;
 import java.io.File;
 import java.io.IOException;
 import javax.imageio.ImageIO;
-import javax.sound.sampled.AudioInputStream;
-import javax.sound.sampled.AudioSystem;
-import javax.sound.sampled.Clip;
 
 /** Displays the Main menu panel
- * @author Éric YU
+ * @author Eric YU
  * @contributor Cédric ABDELBAKI
  * 				- Added : javadoc
  * 				- Added : menuBackground attribute
- * 				- Added : setFontSize() method
+ * 				- Added : replacePanel() method
  * 				- Added : defineButtonsStyle() method
  * 				- Added : paintComponent() method
- * 				- Added : playSound() method
+ * 				- Added : MouseListener methods
  * 				- Modified : javadoc
  * 				- Modified : MenuPanel() constructor
- * 				- Modified : BtnListener internal class
+ * 				- Deleted : BtnListener internal class
  * 				- Deleted : MenuButton class
+ * @version 0.3
  */
-public class MenuPanel extends CustomPanel {
+public abstract class MenuPanel extends CustomPanel implements MouseListener{
 
 	// The menu background image
 	private Image menuBackground;
 
-	/** Creates a new menu panel with a logo, a background, a version, buttons and music 
+	// The main GridBagConstraints object
+	protected GridBagConstraints gbc;
+
+	// The object containing the commands to use in the game
+	protected CommandsMap gameCommands;
+
+	/** Constructor : Allows to create a MenuPanel object
 	 * @param pm the related app window
 	 */
 	public MenuPanel(PanelMediator pm) {
-
 		super(pm);
-		pm.setMenuPanel(this);
 
-		// Defines the menu panel borders, layout and size
-		setBorder(new EmptyBorder(Constants.BORDER_CONST, Constants.BORDER_CONST, Constants.BORDER_CONST, Constants.BORDER_CONST));
-		setLayout(new GridBagLayout());
-		setPreferredSize(Constants.PANEL_DIMENSIONS);
-
-		// Creates grid bag constraints
-		GridBagConstraints gbc = new GridBagConstraints();
-
-		// Defines grid bag constraints parameters
-		gbc.gridwidth = GridBagConstraints.REMAINDER;
-		gbc.anchor = GridBagConstraints.NORTHWEST;
-		gbc.fill = GridBagConstraints.NONE;
-		gbc.weightx = 1.0;
-
-		// Plays the music theme
-		playSound(Constants.THEME_PATH);
-
-		// Creates the title label
-		JLabel titleLabel = new JLabel();
-
-		// Assigns a Pixel Adventurer logo to the label
-		ImageIcon titleImage = new ImageIcon(Constants.LOGO_PATH);
-		titleLabel.setIcon(titleImage);
-
-		// Adds the title logo to the menu panel
-		add(titleLabel, gbc);
-
-		// Creates the version label
-		JLabel versionLabel = new JLabel("Version: " + Constants.GAME_VERSION);
-
-		// Sets the version font size
-		Font versionFont = setFontSize(12);
-
-		// Defines the version title text color and font
-		versionLabel.setFont(versionFont);
-		versionLabel.setForeground(Color.WHITE);
-
-		// Modifies gbc parameters
-		gbc.anchor = GridBagConstraints.SOUTHWEST;
-
-        // Adds the version label to the menu panel
-		add(versionLabel, gbc);
-
-		// Creates the buttons panel
-		JPanel buttonsPanel = new JPanel(new GridBagLayout());
-
-		// Removes the buttons panel background color
-		buttonsPanel.setOpaque(false);
-
-		// The button to switch to the game panel
-		JButton playBtn = new JButton("PLAY");
-
-		// The button to switch to the commands panel
-		JButton cmdsBtn = new JButton("COMMANDS");
-
-		// The button to switch to the credits panel
-		JButton cdtsBtn = new JButton("CREDITS");
-
-		// The button to exit the program
-		JButton quitBtn = new JButton("QUIT");
-
-		// Adds listeners to the buttons
-		playBtn.addMouseListener(new BtnListener());
-		cmdsBtn.addMouseListener(new BtnListener());
-		cdtsBtn.addMouseListener(new BtnListener());
-		quitBtn.addMouseListener(new BtnListener());
-
-		// Adds the buttons to the buttons panel
-		buttonsPanel.add(playBtn, gbc);
-		buttonsPanel.add(cmdsBtn, gbc);
-		buttonsPanel.add(cdtsBtn, gbc);
-		buttonsPanel.add(quitBtn, gbc);
-
-		// Sets the buttons font size
-		Font buttonsFont = setFontSize(24);
-
-		// Defines the buttons panel style
-		defineButtonsStyle(buttonsPanel, buttonsFont);
-
-		// Modifies gbc parameters
-		gbc.anchor = GridBagConstraints.SOUTHEAST;
-		gbc.weighty = 1;
-
-		// Adds the buttons panel to the menu panel
-		add(buttonsPanel, gbc);
-	}
-
-	/** Sets the font size as defined in parameters
-	 * @param fontSize the desired font size
-	 * @return a new Font object
-	 */
-	public Font setFontSize(int fontSize) {
-		return new Font("Atari", Font.PLAIN, fontSize);
-	}
-
-	/** Defines the buttons style
-	 * @author 
-	 * @param buttonsPanel The JPanel containing the buttons
-	 */
-	public void defineButtonsStyle(JPanel buttonsPanel, Font buttonsFont) {
-
-		// Sets a list of buttons components
-		Component[] panelButtons = buttonsPanel.getComponents();
-
-		// Loops on the buttons components
-		for (Component currentButton : panelButtons) {
-
-			// Checks if the button component is an instance of the JButton class
-			if (currentButton instanceof JButton) {
-
-				// Casts the currentButton component to the JButton type
-				JButton button = (JButton) currentButton;
-
-				// Sets the button font
-				button.setFont(buttonsFont);
-
-				// Sets the button text in black
-				button.setForeground(Color.WHITE);
-
-				// Removes the borders from the button
-				button.setBorder(null);
-
-				// Removes the background from the button
-				button.setContentAreaFilled(false);
-
-				// Aligns the buttons to the left
-				button.setHorizontalAlignment(SwingConstants.LEFT);
-			}
+		// Creates a CommandsMap object if it does not already exists
+		if (gameCommands == null) {
+			gameCommands = new CommandsMap();
 		}
-	}
-
-	// Draws the menu background image
-	@Override
-	protected void paintComponent(Graphics g) {
-		super.paintComponent(g);
 
 		// Tries to read the background image file
 		try {
@@ -193,106 +63,94 @@ public class MenuPanel extends CustomPanel {
 			System.err.println("Error trying to read the menu background image file" + e);
 		}
 
-		// Gets the panel width and height
+		// Defines the panel borders and size
+		setBorder(new EmptyBorder(Constants.BORDER_CONST, Constants.BORDER_CONST, Constants.BORDER_CONST, Constants.BORDER_CONST));
+		setPreferredSize(Constants.PANEL_DIMENSIONS);
+
+		// Defines the layout
+		setLayout(new GridBagLayout());
+		this.gbc = new GridBagConstraints();
+
+		// Creates and adds the Pixel Adventurer lol
+		JLabel pixelAdventurerLogoLabel = new JLabel();
+		ImageIcon pixelAdventurerLogoIcon = new ImageIcon(Constants.LOGO_PATH);
+		pixelAdventurerLogoLabel.setIcon(pixelAdventurerLogoIcon);
+		gbc.gridwidth = GridBagConstraints.REMAINDER;
+		gbc.anchor = GridBagConstraints.NORTHWEST;
+		gbc.fill = GridBagConstraints.NONE;
+		gbc.weightx = 1.0;
+		add(pixelAdventurerLogoLabel, gbc);
+
+		// Creates and adds the game version label
+		JLabel versionLabel = new JLabel("Version: " + Constants.GAME_VERSION);
+		Font versionFont = new Font("Atari", Font.PLAIN, 12);
+		versionLabel.setFont(versionFont);
+		versionLabel.setForeground(Color.WHITE);
+		gbc.anchor = GridBagConstraints.SOUTHWEST;
+		add(versionLabel, gbc);
+	}
+
+	/** Defines the buttons style
+	 * @author 
+	 * @param buttonsPanel The JPanel containing the buttons
+	 */
+	public void defineButtonStyle(JButton button) {
+		Font buttonFont = new Font("Atari", Font.PLAIN, 24);
+		button.setFont(buttonFont);
+		button.setForeground(Color.WHITE);
+		button.setBorder(null);
+		button.setContentAreaFilled(false);
+		button.setHorizontalAlignment(SwingConstants.LEFT);
+	}
+
+	/** Removes a panel from the window and adds a new one
+	 * @param gameWindow The window in which you want to change the panel
+	 * @param panel The panel you want to add
+	 */
+	protected void replacePanel(JFrame gameWindow, CustomPanel panel) {
+		try {
+			// Removes the menu panel from the window
+			gameWindow.remove(this);	
+			gameWindow.add(panel);
+			gameWindow.toFront();
+			gameWindow.requestFocus();
+		} catch (NullPointerException exception) {
+			System.out.print(exception.getMessage());
+		}
+	}
+
+	// Draws the menu background image
+	@Override
+	protected void paintComponent(Graphics g) {
+		super.paintComponent(g);
 		int width = getSize().width;
 		int height = getSize().height;
-
-		// Draws the menu background image at position 0,0 with the panel dimensions
 		g.drawImage(menuBackground, 0, 0, width, height, this);
 	}
 
-	// Plays a sound whose file path is specified as a parameter
-    public void playSound(String soundFilePath) {
+	// Modifies the button text color to yellow when hovered
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		JButton hoveredButton = (JButton) e.getSource();
+		hoveredButton.setForeground(Color.decode("#FFDE59"));
+	}
 
-    	// Tries to open and play the sound
-        try {
-            File audioFile = new File(soundFilePath);
-            AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(audioFile);
-            Clip audioClip = AudioSystem.getClip();
-            audioClip.open(audioInputStream);
-            audioClip.loop(-1);
-        } catch (Exception e) {
-            System.err.println("Error trying to read and play the audio file" + e);
-        }
-    }
+	// Modifies the button text color to white when exited
+	@Override
+	public void mouseExited(MouseEvent e) {
+		JButton exitedButton = (JButton) e.getSource();
+		exitedButton.setForeground(Color.WHITE);	
+	}
 
-    /** Implements the MouseListener methods to handle mouse behavior
-     * @author
-     */
-    public class BtnListener implements MouseListener {
+	@Override
+	public void mouseClicked(MouseEvent e) {
+	}
 
-    	// Defines actions when a button is clicked
-		@Override
-		public void mouseClicked(MouseEvent e) {
+	@Override
+	public void mousePressed(MouseEvent e) {
+	}
 
-			JFrame gameWindow = ApplicationWindow.getFrame();
-			JButton clickedButton = (JButton) e.getSource();
-			String buttonText = clickedButton.getActionCommand();
-
-			// Switch case structure to select the action depending on the button clicked on
-			switch (buttonText) {
-
-			// Starts a new game
-			case "PLAY":
-				try {
-
-					// Removes the menu panel from the window
-					gameWindow.remove(MenuPanel.this);
-
-					// Adds the game panel instead
-					gameWindow.add(pm.getGamePanel());
-
-					// Brings focus on the window
-					gameWindow.toFront();
-					gameWindow.requestFocus();
-
-				} catch (NullPointerException exception) {
-					System.out.print(exception.getMessage());
-				}
-				break;
-
-			// Displays the commands menu
-			case "COMMANDS":
-				break;
-
-			// Displays the game credits
-			case "CREDITS":
-				break;
-
-			// Exits the game
-			case "QUIT":
-				gameWindow.dispose();
-				break;
-			}
-
-			// Updates the game window after changes
-			gameWindow.revalidate();
-			gameWindow.repaint();
-		}
-
-		// Modifies the button text color to yellow when hovered
-		@Override
-		public void mouseEntered(MouseEvent e) {
-			JButton hoveredButton = (JButton) e.getSource();
-			hoveredButton.setForeground(Color.decode("#FFDE59"));
-		}
-
-		// Modifies the button text color to white when exited
-		@Override
-		public void mouseExited(MouseEvent e) {
-			JButton exitedButton = (JButton) e.getSource();
-			exitedButton.setForeground(Color.WHITE);	
-		}
-
-		@Override
-		public void mousePressed(MouseEvent e) {
-			// TODO Auto-generated method stub
-		}
-
-		@Override
-		public void mouseReleased(MouseEvent e) {
-			// TODO Auto-generated method stub
-		}
-    }
+	@Override
+	public void mouseReleased(MouseEvent e) {
+	}
 }
-
