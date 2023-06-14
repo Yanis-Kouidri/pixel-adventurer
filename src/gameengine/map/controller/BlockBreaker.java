565261a2709a7d17b091ad9bc26e97b12858ac51
jtheret <jtheret@inp-toulouse.fr>
2023-06-14 11:42:23 +0200

    Add author

--- src/gameengine/map/controller/BlockBreaker.java
+++ src/gameengine/map/controller/BlockBreaker.java
@@ -14,6 +14,7 @@ import java.util.Objects;
 
 /**
  * Determine the block to break, destroy it and add the block as an item to the inventory.
+ * @author Jeremy Theret
  */
 public class BlockBreaker extends MouseAdapter {
 
