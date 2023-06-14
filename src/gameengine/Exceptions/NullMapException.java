4f852ef2b412617a15bf811d6415a6d4c922157f
thomas <thomas.gruner@etu.inp-n7.fr>
2023-06-14 09:30:04 +0200

    feat : missing javadoc implementation

--- src/gameengine/Exceptions/NullMapException.java
+++ src/gameengine/Exceptions/NullMapException.java
@@ -8,7 +8,11 @@ package gameengine.Exceptions;
 public class NullMapException extends RuntimeException{
 
 	private static final long serialVersionUID = 1L;
-
+	
+	/**
+	 * NullMapException constructor
+	 * @param message
+	 */
 	public NullMapException(String message) {
 		super(message);
 	}
